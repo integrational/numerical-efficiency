5d063452adb719d7f6f445fbff051a473756b247
GeraldLoeffler <gerald.loeffler@googlemail.com>
2021-05-14 19:16:33 +0200

    removed some unnecessary Crystal type annotations

--- crystal-by-value/square_root.cr
+++ crystal-by-value/square_root.cr
@@ -33,14 +33,14 @@ def all_sqrts(reps : Int32, step : Int32, e_max : Float64)
   assert_accum_devs dev, e_max
 end
 
-def assert_correctness(x : Float64, s : Float64, e_max : Float64)
+def assert_correctness(x, s, e_max)
 	c = Math.sqrt(s)
 	e = (c - x).abs
 	e_max_safe = e_max * 1.01 # add 1% safety margin because error used during calculation is an approximation
   raise "sqrt(#{s}): deviation #{e} between correct result #{c} and approximation #{x} exceeds maximally allowed error #{e_max}" unless e <= e_max_safe
 end
 
-def assert_accum_devs(d : Result, e_max : Float64)
+def assert_accum_devs(d, e_max)
   raise "Accumulated deviations exceed acceptable thresholds" unless d.n == 0 && d.x <= e_max && d.e <= e_max
 end
 
@@ -60,7 +60,7 @@ def sqrt_by_recursion(s : Float64, e_max : Float64) : Result
   Result.new r.n, r.x, r.e.abs
 end
 
-def sqrt_by_recursion(s : Float64, e_max : Float64, r : Result) : Result
+def sqrt_by_recursion(s, e_max, r)
   if r.e.abs <= e_max
     r
   else
@@ -70,17 +70,17 @@ def sqrt_by_recursion(s : Float64, e_max : Float64, r : Result) : Result
   end
 end
 
-def initial(s : Float64, e_max : Float64) : Result
+def initial(s, e_max)
   Result.new 0, 
     e_max,          # terrible choice
     error(s, e_max) # for x = e_max
 end
 
-def error(s : Float64, x : Float64) : Float64
+def error(s, x)
   (s - x*x)/(2*x)
 end
 
-def improve(x : Float64, e : Float64) : Float64
+def improve(x, e)
   x + e
 end
 
